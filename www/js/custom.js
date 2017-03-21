bec4ab4bd37d27155b3c9fe89f350ea6cbc9a6b7
Dipezrok <Biel_srp@hotmail.com>
2017-03-20 23:44:30 -0300

    Algumas criações na register choose e adição plugin JS de masks, tentativa de por as masks

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,3 +1,27 @@
 $(document).on('click', '.categoria-icon', function(){
     $("p", this).toggleClass('selected');
 })
+
+
+/* Cadastros */ 
+
+/*
+$(document).focus('#telefone', function(){
+    $("#telefone").mask("(99)999-9999");
+   
+});
+
+
+jQuery(function($){
+   $("#date").mask("99/99/9999",{placeholder:"mm/dd/yyyy"});
+   $("#telefone").mask("(999)999-9999");
+   $("#tin").mask("99-9999999");
+   $("#ssn").mask("999-99-9999");
+});
+
+jQuery(function($){
+   $("#telefone").mask("(99) 999-9999");
+});
+
+    
+*/

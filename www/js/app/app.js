abde91f1c058e408a4648a88e15f0a2aec0f0e8d
Davi Augusto <Davi Augusto>
2017-04-20 22:06:21 -0300

    Organização dos diretórios da aplicação

---
+++ www/js/app/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('coletivando', ['ionic', 'firebase', 'ionic.cloud'])
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
